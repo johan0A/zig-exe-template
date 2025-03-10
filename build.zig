e8b8b067a097715477baaec142fefdb942bf2974
johan0A <arnaudchesse@outlook.com>
2025-03-10 16:01:33 +0100

    update

--- build.zig
+++ build.zig
@@ -4,38 +4,29 @@ const B = std.Build;
 pub fn build(b: *B) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const name = b.option([]const u8, "name", "set the name of the emitted binary");
 
-    const name = b.option([]const u8, "name", "set the name of the outputted artifact");
+    const root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     {
-        const exe = b.addExecutable(.{
-            .name = name orelse "zig-exe-template",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(exe, b, target, optimize);
+        const exe = b.addExecutable(.{ .name = name orelse "zig-exe-template", .root_module = root_module });
 
         b.installArtifact(exe);
         const run_cmd = b.addRunArtifact(exe);
         run_cmd.step.dependOn(b.getInstallStep());
 
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
+        if (b.args) |args| run_cmd.addArgs(args);
 
         const run_step = b.step("run", "Run the app");
         run_step.dependOn(&run_cmd.step);
     }
 
     {
-        const tests = b.addTest(.{
-            .name = name orelse "test",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(tests, b, target, optimize);
+        const tests = b.addTest(.{ .name = name orelse "test", .root_module = root_module });
 
         const run_tests = b.addRunArtifact(tests);
         const test_step = b.step("test", "Run unit tests");
@@ -47,36 +38,11 @@ pub fn build(b: *B) void {
     }
 
     {
-        const exe_check = b.addExecutable(.{
-            .name = "check",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(exe_check, b, target, optimize);
-
-        const tests_check = b.addTest(.{
-            .name = "check",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(tests_check, b, target, optimize);
+        const exe_check = b.addExecutable(.{ .name = "check", .root_module = root_module });
+        const tests_check = b.addTest(.{ .name = "check", .root_module = root_module });
 
         const check = b.step("check", "Check if exe and tests compile");
         check.dependOn(&exe_check.step);
         check.dependOn(&tests_check.step);
     }
-}
-
-fn addDependencies(
-    compile_step: *B.Step.Compile,
-    b: *B,
-    target: B.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-) void {
-    _ = compile_step;
-    _ = b;
-    _ = target;
-    _ = optimize;
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
+    var a: u32 = 0;
+    a += 1;
+    a += 1;
+    a += 1;
+    a += 1;
+    a += 1;
+    a += 1;
+
     std.debug.print("Hello, world!\n", .{});
 }
