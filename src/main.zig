46ad821c0290f895fe88320e9f4a14dd4516898e
johan0A <arnaudchesse@outlook.com>
2025-03-10 17:59:39 +0100

    update

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,5 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var a: u32 = 0;
-    a += 1;
-    a += 1;
-    a += 1;
-    a += 1;
-    a += 1;
-    a += 1;
-
     std.debug.print("Hello, world!\n", .{});
 }
